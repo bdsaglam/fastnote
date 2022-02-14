0434b1ba6ff40f73a86ace90204b1a226a733103
Barış Deniz Sağlam <bdsaglam@gmail.com>
2022-02-14 12:08:08 +0300

    implement binary focal loss

--- fastnote/_nbdev.py
+++ fastnote/_nbdev.py
@@ -2,9 +2,11 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"find_binary_clf_threshold": "ml.evaluation.ipynb"}
+index = {"find_binary_clf_threshold": "ml.evaluation.ipynb",
+         "BinaryFocalLoss": "ml.loss.ipynb"}
 
-modules = ["ml/evaluation.py"]
+modules = ["ml/evaluation.py",
+           "ml/loss.py"]
 
 doc_url = "https://bdsaglam.github.io/fastnote/"
 
